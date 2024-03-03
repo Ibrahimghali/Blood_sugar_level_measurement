ab2fb571117edadce1513ac81021cd1d8bdabb58
Ibrahim <ghaliibrahim60@gmail.com>
2024-03-03 18:11:10 +0100

    First Version

---
+++ GlicemicCalculator/app/src/main/java/com/example/glicemiccalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.glicemiccalculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Connect the activity to the layout
+    }
+}
---
+++ GlicemicCalculator/app/src/main/java/com/example/glicemiccalculator/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.example.glicemiccalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+}
